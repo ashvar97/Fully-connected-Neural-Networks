da85716639e8fcfa9aec856754ab0afacc732399
ashvar97 <113519947+ashvar97@users.noreply.github.com>
2025-02-24 12:50:40 +0100

    Delete Optimizers.py

--- Optimizers.py
+++
@@ -1,7 +0,0 @@
-class Sgd:
-    def __init__(self, learning):
-        self.learning_rate = learning
-
-    def calculate_update(self, weight_tensor, gradient_tensor):
-        updated_wts = weight_tensor - self.learning_rate * gradient_tensor
-        return updated_wts
